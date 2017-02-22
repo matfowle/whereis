a70c529872d6fc148c67523fa0a5ac3e4c505c53
Matt Fowler <matfowle@cisco.com>
2017-02-22 18:19:40 +1100

    minor changes

--- cgi-bin/whereis.py
+++ cgi-bin/whereis.py
@@ -1,7 +1,37 @@
 #!/usr/local/bin/python
+############################################################################
+#                                                                          #
+#   Module:         whereis.py                                             #
+#   Author:         Matthew Fowler 2017                                    #
+#   Company:        Cisco Systems                                          #
+#   Description:    Python Spark Bot that listens for input in a Spark     #
+#                   room and then submits the input as a username to       #
+#                   the main python script that gets the users location    #
+#                   from CMX. It then posts the returned info back to the  #
+#                   room.                                                  #
+#                                                                          #
+############################################################################
+
+############################################################################
+#                       Dependancy Imports                                 #
+############################################################################
+
 from itty import *
 from requests_toolbelt import MultipartEncoder
 import urllib2, json, requests, os, sys
+
+############################################################################
+#                       Environment Settings                               #
+############################################################################
+
+####CHANGE THESE VALUES#####
+bearer = ""
+bot_email = ""
+cmx_script_url = ""
+
+############################################################################
+#                       Function Definitions                               #
+############################################################################
 
 def sendSparkGET(url):
     request = urllib2.Request(url,
@@ -17,6 +47,10 @@ def sendSparkPOST(url, data):
                             headers={"Content-Type": m.content_type,
                             "Authorization": "Bearer "+bearer})
     return request
+
+############################################################################
+#                         Main Execution                                   #
+############################################################################
 
 @post('/')
 def index(request):
@@ -64,9 +98,5 @@ def index(request):
 
     return "true"
 
-####CHANGE THESE VALUES#####
-bearer = ""
-bot_email = ""
-cmx_script_url = ""
 
 run_itty(server='wsgiref', host='127.0.0.1', port=10010)
